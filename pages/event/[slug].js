47983c464c820a7b47352e7f1f9219b17dbc4f17
John Lanni <Johnlanni619@gmail.com>
2021-11-17 16:13:10 -0500

    added Links

---
+++ pages/event/[slug].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+import Layout from "../../components/Layout"
+
+export default function slug() {
+    const router = useRouter();
+    const { slug } = router.query;
+
+    return(
+        <Layout>
+            <h1>{slug}</h1>
+        </Layout>
+    )
+}
